8492905945d89283801c0bd2f6a38b19477c6380
TengkuRafi <63381758+TengkuRafi@users.noreply.github.com>
2021-10-01 13:17:20 +0700

    Mengerjakan Vidio 3 Tentang Routing
    Mengerjakan Vidio 3 Tentang Routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,29 @@
 <?php
+/*
+Tengku Muhammad Rafi
+193040091
+https://github.com/TengkuRafi/prakweb2021_phpmvc_193040091
+Pertemuan3
+Mengerjakan MVC Vidio 3
+1 Oktober 2021
+*/
+
 
 class App {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this-> parseURL();
+        var_dump($url);
     }
 
+    public function parseURL()
+    {
+        if( isset($_GET['url']) ) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);   
+            return $url;
+        }
+    }
 
 }
